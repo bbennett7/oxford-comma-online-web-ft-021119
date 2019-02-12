26805d1397cf27fc47dab702f318e553ce823263
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-12 23:25:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     new_string << array.join(" and ")
   else
     new_string << array.join(", ")
-    length_from_end = array.length - array.last.length
+    length_from_end = new_string.length - array[-1].length
     new_string.insert(length_from_end, "and ")
   end
   new_string
